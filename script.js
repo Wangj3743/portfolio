e68555b3274bffab32bf0b06b3b6048e4cbf6690
Jim Wang <75101157+Wangj3743@users.noreply.github.com>
2022-12-17 22:43:17 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function email1() {document.getElementById("email").style.color = '#fbbc04';}
+function email2() {document.getElementById("email").style.color = 'azure';}
+
+function git1() {document.getElementById("git").style.color = '#e84d31';}
+function git2() {document.getElementById("git").style.color = 'azure';}
+
+function link1() {document.getElementById("link").style.color = '#0077b5';}
+function link2() {document.getElementById("link").style.color = 'azure';}
